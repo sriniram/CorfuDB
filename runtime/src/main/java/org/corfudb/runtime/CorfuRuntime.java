5be76a4327b9ed07a264156a6f38d96946738cb9
lixinchengdu <lixinchengdu@gmail.com>
2019-11-07 13:28:51 -0800

    Trim garbage at runtime when compactionMark is updated. (#2175)

--- runtime/src/main/java/org/corfudb/runtime/CorfuRuntime.java
+++ runtime/src/main/java/org/corfudb/runtime/CorfuRuntime.java
@@ -569,6 +569,9 @@ public class CorfuRuntime {
     @Getter
     public volatile UUID clusterId;
 
+    @Getter
+    final ViewsGarbageCollector garbageCollector = new ViewsGarbageCollector(this);
+
     /**
      * Notifies that the runtime is no longer used
      * and async retries to fetch the layout can be stopped.
@@ -748,6 +751,8 @@ public class CorfuRuntime {
         isShutdown = true;
         runtimeExecutor.shutdownNow();
         garbageInformer.stop();
+        garbageCollector.stop();
+
         if (layout != null) {
             try {
                 layout.cancel(true);
